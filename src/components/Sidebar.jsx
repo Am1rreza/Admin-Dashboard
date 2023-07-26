1c6302363ff97a3136e98e1a3d38cd98ab362055
Amirreza <amirreza.m13825@gmail.com>
2023-07-26 18:36:59 +0330

    set the fanctionalities in the sidebar

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -20,7 +20,7 @@ const Sidebar = () => {
           <div className="flex justify-between items-center">
             <Link
               to={"/"}
-              onClick={() => {}}
+              onClick={() => setActiveMenu(false)}
               className="items-center gap-3 ml-3 mt-4 flex text-xl font-extrabold tracking-tight dark:text-white text-slate-900"
             >
               <SiShopware /> <span>Shoppy</span>
@@ -28,7 +28,7 @@ const Sidebar = () => {
             <TooltipComponent content="Menu" position="BottomCenter">
               <button
                 type="button"
-                onClick={() => {}}
+                onClick={() => setActiveMenu((prevState) => !prevState)}
                 className="text-xl rounded-full p-3 hover:bg-light-gray mt-4 block md:hidden"
               >
                 <MdOutlineCancel />
