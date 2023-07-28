0e9a860474bd36b21bc03f4e7862c6a0c45c3c89
Amirreza <amirreza.m13825@gmail.com>
2023-07-28 15:05:07 +0330

    button component created

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,5 +1,13 @@
-const Button = () => {
-  return <>Button</>;
+const Button = ({ bgColor, color, size, text, borderRadius }) => {
+  return (
+    <button
+      type="button"
+      style={{ backgroundColor: bgColor, color, borderRadius }}
+      className={`text-${size} p-3 hover:drop-shadow-lg`}
+    >
+      {text}
+    </button>
+  );
 };
 
 export default Button;
