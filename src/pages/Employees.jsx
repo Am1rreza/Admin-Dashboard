a08a0233db03b212df152c8cbb49bb4c2862736a
Amirreza <amirreza.m13825@gmail.com>
2023-08-06 11:12:31 +0330

    fix styles

--- src/pages/Employees.jsx
+++ src/pages/Employees.jsx
@@ -12,7 +12,7 @@ import { Header } from "../components";
 
 const Employees = () => {
   return (
-    <div className="m-2 md:m-10 p-2 md:p-10 bg-white rounded-3xl">
+    <div className="m-2 md:m-10 mt-24 p-4 md:p-10 bg-white rounded-3xl">
       <Header category="Page" title="Employees" />
       <GridComponent
         dataSource={employeesData}
