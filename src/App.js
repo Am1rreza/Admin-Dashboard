66417c5325b6316453dd65dff582a1e67ef8a634
Amirreza <amirreza.m13825@gmail.com>
2023-08-08 16:00:02 +0330

    implement dark mode

--- src/App.js
+++ src/App.js
@@ -25,11 +25,16 @@ import { TooltipComponent } from "@syncfusion/ej2-react-popups";
 import { useStateContext } from "./contexts/ContextProvider";
 
 function App() {
-  const { activeMenu, themeSettings, setThemeSettings, currentColor } =
-    useStateContext();
+  const {
+    activeMenu,
+    themeSettings,
+    setThemeSettings,
+    currentMode,
+    currentColor,
+  } = useStateContext();
 
   return (
-    <div>
+    <div className={currentMode === "Dark" ? "dark" : ""}>
       <BrowserRouter>
         <div className="flex relative dark:bg-main-dark-bg">
           <div className="fixed right-4 bottom-4" style={{ zIndex: "1000" }}>
@@ -54,7 +59,7 @@ function App() {
             </div>
           )}
           <div
-            className={`dark:bg-main-bg bg-main-bg min-h-screen w-full ${
+            className={`dark:bg-main-dark-bg bg-main-bg min-h-screen w-full ${
               activeMenu ? `md:ml-72` : `flex-2`
             }`}
           >
