a5dd80c72922b81f56ba72d0ffeb34c55412b417
Amirreza <amirreza.m13825@gmail.com>
2023-08-08 15:31:08 +0330

    update context settings

--- src/contexts/ContextProvider.js
+++ src/contexts/ContextProvider.js
@@ -13,6 +13,18 @@ export const ContextProvider = ({ children }) => {
   const [activeMenu, setActiveMenu] = useState(true);
   const [isClicked, setIsClicked] = useState(initialState);
   const [screenSize, setScreenSize] = useState(undefined);
+  const [currentColor, setCurrentColor] = useState("#03C9D7");
+  const [currentMode, setCurrentMode] = useState("Light");
+
+  const setMode = (e) => {
+    setCurrentMode(e.target.value);
+    localStorage.setItem("themeMode", e.target.value);
+  };
+
+  const setColor = (e) => {
+    setCurrentColor(e.target.value);
+    localStorage.setItem("colorMode", e.target.value);
+  };
 
   const handleClick = (clicked) => {
     setIsClicked({ ...initialState, [clicked]: true });
@@ -28,6 +40,10 @@ export const ContextProvider = ({ children }) => {
         handleClick,
         screenSize,
         setScreenSize,
+        currentColor,
+        currentMode,
+        setCurrentColor,
+        setCurrentMode,
       }}
     >
       {children}
