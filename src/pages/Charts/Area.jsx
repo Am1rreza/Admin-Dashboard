fdf1f5e7d37e90b8d2afe7ba7ed0c3a1ee1b7170
Amirreza <amirreza.m13825@gmail.com>
2023-08-13 18:49:21 +0330

    create the area chart

--- src/pages/Charts/Area.jsx
+++ src/pages/Charts/Area.jsx
@@ -1,5 +1,45 @@
+import {
+  ChartComponent,
+  SeriesCollectionDirective,
+  SeriesDirective,
+  Inject,
+  DateTime,
+  Legend,
+  SplineAreaSeries,
+} from "@syncfusion/ej2-react-charts";
+import {
+  areaCustomSeries,
+  areaPrimaryXAxis,
+  areaPrimaryYAxis,
+} from "../../data/dummy";
+import { useStateContext } from "../../contexts/ContextProvider";
+import { Header } from "../../components";
+
 const Area = () => {
-  return <>Area</>;
+  const { currentMode } = useStateContext();
+
+  return (
+    <div className="m-4 md:m-10 mt-24 p-10 bg-white dark:bg-secondary-dark-bg rounded-3xl">
+      <Header category="Area" title="Inflation Rate in Percentage" />
+      <ChartComponent
+        id="line-chart"
+        height="420px"
+        primaryXAxis={areaPrimaryXAxis}
+        primaryYAxis={areaPrimaryYAxis}
+        chartArea={{ border: { width: 0 } }}
+        tooltip={{ enable: true }}
+        background={currentMode === "Dark" ? "#33373e" : "#fff"}
+      >
+        <Inject services={[SplineAreaSeries, DateTime, Legend]} />
+        <SeriesCollectionDirective>
+          {areaCustomSeries.map((item, index) => {
+            return <SeriesDirective key={index} {...item} />;
+          })}
+          s
+        </SeriesCollectionDirective>
+      </ChartComponent>
+    </div>
+  );
 };
 
 export default Area;
